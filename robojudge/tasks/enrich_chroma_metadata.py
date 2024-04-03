2b37428fa9e892f3fe5c19c26b1971d67da6143e
Miroslav Rehounek <rehounek.miroslav@gmail.com>
2024-04-03 19:27:21 +0200

    Fix migrate script

--- robojudge/tasks/enrich_chroma_metadata.py
+++ robojudge/tasks/enrich_chroma_metadata.py
@@ -1,20 +1,31 @@
 import asyncio
 import multiprocessing.pool
-
+from pathlib import Path
 
 from robojudge.db.mongo_db import document_db
 from robojudge.db.chroma_db import embedding_db
 from robojudge.utils.logger import logger
+
+MEMORY_PATH = Path("/var/migration_successful")
 
 
 async def run_migration():
     """
     One time migration that adds date metadata to Chroma entries.
     """
+    if MEMORY_PATH.exists():
+        logger.info(
+            f"Migration skipped because it was already performed. Delete {MEMORY_PATH} file if you want to repeat it."
+        )
+        return
+
     pool = multiprocessing.pool.ThreadPool(processes=1)
 
     rulings = document_db.collection.find()
     pool.map(migrate_ruling, rulings)
+
+    with open(MEMORY_PATH, "w") as wf:
+        wf.write("done")
 
     logger.info("Migration is finished.")
 
@@ -22,24 +33,24 @@ async def run_migration():
 
 
 def migrate_ruling(ruling: dict):
-    ruling_chunks = embedding_db.parse_text_query_result(
-        embedding_db.collection.query(
+    # ruling_chunks = embedding_db.get_case_chunks_by_case_id(case_ids=[ruling["case_id"]])
+    try:
+        ruling_chunks = embedding_db.collection.query(
             query_texts="", where={"case_id": {"$in": [ruling["case_id"]]}}
         )
-    )
 
-    for metadata in ruling_chunks["metadatas"]:
-        metadata["sentence_date"] = ruling["metadata"]["sentence_date"].timestamp()
-        metadata["publication_date"] = ruling["metadata"][
-            "publication_date"
-        ].timestamp()
-        metadata["court"] = ruling["metadata"]["court"]
-    try:
-        embedding_db.collection.update(
-            ids=ruling_chunks["ids"], metadatas=ruling_chunks["metadatas"]
-        )
+        for metadata in ruling_chunks["metadatas"][0]:
+            metadata["sentence_date"] = ruling["metadata"]["sentence_date"].timestamp()
+            metadata["publication_date"] = ruling["metadata"][
+                "publication_date"
+            ].timestamp()
+            metadata["court"] = ruling["metadata"]["court"]
+
+            embedding_db.collection.update(
+                ids=ruling_chunks["ids"][0], metadatas=ruling_chunks["metadatas"][0]
+            )
     except Exception as e:
-        logger.info(f'Error while updating chunks {ruling_chunks.get("ids", [])}: {e}')
+        logger.info(f'Error while updating chunks: {e}')
 
     logger.info(f'Updated chunks for ruling with id {ruling["case_id"]}.')
 
